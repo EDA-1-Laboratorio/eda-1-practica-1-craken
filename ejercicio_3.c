ad798556907ca5c330423f8c4e536305fe9538aa
JoseGutierrez101 <jpkogutierrez@gmail.com>
2026-02-12 18:43:17 -0600

    Update ejercicio_3.c

--- ejercicio_3.c
+++ ejercicio_3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> // Para la función sleep() en Linux/Mac (usar <windows.h> y Sleep() en Windows)
+//#include <windows.h>
 
 #define FILAS 10
 #define COLS 10
 
+int selec;
 // 0 = Muerta, 1 = Viva
 int mundo[FILAS][COLS];
 int siguiente_mundo[FILAS][COLS];
@@ -16,18 +18,30 @@ void inicializarMundo() {
         for(int j=0; j<COLS; j++) 
             mundo[i][j] = 0;
 
+            printf("\nSeleccione opcion: \n (1)Glider \n(otro)Aleatorio \t");
+        scanf("%i", &selec);
+
+    if (selec == 1) {
     // Patrón "Glider" (Deslizador) simple para pruebas
-    mundo[1][2] = 1;
-    mundo[2][3] = 1;
-    mundo[3][1] = 1;
-    mundo[3][2] = 1;
-    mundo[3][3] = 1;
+        mundo[1][2] = 1;
+        mundo[2][3] = 1;
+        mundo[3][1] = 1;
+        mundo[3][2] = 1;
+        mundo[3][3] = 1;
+    } else {
+        srand(selec);
+            for (int i = 0; i<FILAS; i++) {
+                for (int j = 0; j<COLS; j++) {
+                    if (rand()%100 <= 25) mundo[i][j] = 1;
+                 }
+            }
+        }
 }
 
 void imprimirMundo() {
     // Limpia la consola
-    // system("clear"); // O system("cls") en Windows
-    
+    system("clear"); // O system("cls") en Windows
+    // system("cls");
     printf("\nEstado Actual:\n");
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLS; j++) {
@@ -41,6 +55,17 @@ void imprimirMundo() {
 
 int contarVecinos(int f, int c) {
     int vecinos = 0;
+
+     for (int i = -1; i<=1; i++ ) {
+        if (f+i < 0 || f+i > FILAS-1) continue; //Checa limites filas
+        for (int j = -1; j<=1; j++) {
+
+            if (c+j < 0 || c+j > COLS-1) continue; //Checa limites columnas
+            if ((j==0) && (i==0)) continue; //Se omite a si mismo
+
+            if (mundo[f+i][c+j]==1) vecinos++;//Checa si esta viva
+        }
+    }
     
     // TODO: Recorrer las 8 celdas alrededor de mundo[f][c]
     // CUIDADO: Debes verificar que los índices no se salgan del arreglo (0 a FILAS-1)
@@ -74,7 +99,10 @@ void siguienteGeneracion() {
 
             } else {
                 // REGLA 4: Reproducción (exactamente 3 vecinos) -> Nace
-                
+                if (vecinos == 3) {
+                    siguiente_mundo[i][j] = 1;
+                     //printf("%i %i nace", i, j);
+                }
                 // TODO: Completar la lógica para células muertas
             }
         }
@@ -90,12 +118,13 @@ void siguienteGeneracion() {
 
 int main() {
     inicializarMundo();
-    int iteraciones = 5; // O infinito while(1)
+    int iteraciones = 20; // O infinito while(1)
 
     for(int k=0; k<iteraciones; k++) {
         imprimirMundo();
         siguienteGeneracion();
-        // sleep(1); // Pausa para ver la animación
+        sleep(1); // Pausa para ver la animación
+        // Sleep(1);
     }
 
     return 0;
